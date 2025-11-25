07a216586c1a19fda281716c24ff63197b037ee9
Raimdrs <raiimdrs@gmail.com>
2025-11-25 20:59:35 -0300

    feat: Adiciona o metodo simplificado  CalcularCustoTotal

--- src/main/java/ecommerce/service/CompraService.java
+++ src/main/java/ecommerce/service/CompraService.java
@@ -79,9 +79,62 @@ public class CompraService
 		return compraDTO;
 	} 
 
-	public BigDecimal calcularCustoTotal(CarrinhoDeCompras carrinho, Regiao regiao, TipoCliente tipoCliente)
-	{
-		// To-Do
-		return BigDecimal.ZERO;
+	public BigDecimal calcularCustoTotal(CarrinhoDeCompras carrinho, Regiao regiao, TipoCliente tipoCliente){
+		if (carrinho == null|| carrinho.getItens() == null || carrinho.getItens().isEmpty()){
+			return BigDecimal.ZERO;
+		}
+
+		// 1. Calcular Subtotal (Soma: Preço * Quantidade)
+		BigDecimal subtotal = BigDecimal.ZERO;
+		double pesoTotal = 0.0;
+		long quantidadeFrageis = 0;
+
+		for (var item : carrinho.getItens()) {
+			BigDecimal preco = item.getProduto().getPreco();
+			BigDecimal quantidade = BigDecimal.valueOf(item.getQuantidade());
+
+			subtotal = subtotal.add(preco.multiply(quantidade));
+
+			// Acumulara peso físcio total
+			pesoTotal += item.getProduto().getPesoFisico().doubleValue() * item.getQuantidade();
+
+			// Contar itens frágeis
+			if (Boolean.TRUE.equals(item.getProduto().isFragil())) {
+				quantidadeFrageis += item.getQuantidade();
+			}
+		}
+
+		// 2. Aplicar Desconto por Valor Total
+		// >= 1000: 20% | >= 500 e < 1000: 10% | Outros: 0%
+		BigDecimal desconto = BigDecimal.ZERO;
+		if (subtotal.compareTo(BigDecimal.valueOf(1000.00)) >= 0) {
+			desconto = subtotal.multiply(BigDecimal.valueOf(0.20));
+		} else if (subtotal.compareTo(BigDecimal.valueOf(500.00)) >= 0) {
+			desconto = subtotal.multiply(BigDecimal.valueOf(0.10));
+		}
+
+		BigDecimal subtotalComDesconto = subtotal.subtract(desconto);
+
+		// 3. Calcular Frete por Peso
+		BigDecimal Valorfrete = BigDecimal.ZERO;
+
+		if (pesoTotal > 50){
+			Valorfrete = BigDecimal.valueOf(pesoTotal).multiply(new BigDecimal("7.00")); // Faixa D (50 ou mais)
+		} else if (pesoTotal > 10){
+			Valorfrete = BigDecimal.valueOf(pesoTotal).multiply(new BigDecimal("4.00")); // Faixa C (10-50kg)
+		} else if (pesoTotal > 5){
+			Valorfrete = BigDecimal.valueOf(pesoTotal).multiply(new BigDecimal("2.00")); // Faixa B (5-10kg)
+		}
+		// Faixa de A (0-5kg) está isenta de frete
+
+		// 4. Adicionar Taxa para Itens Frágeis (5 reais por item)
+		if (quantidadeFrageis > 0){
+			BigDecimal adicionalFragil = BigDecimal.valueOf(quantidadeFrageis).multiply(new BigDecimal("5.00"));
+			Valorfrete = Valorfrete.add(adicionalFragil);
+		}
+
+		//4. Total Final arrendodado
+		BigDecimal totalFinal = subtotalComDesconto.add(Valorfrete);
+		return totalFinal.setScale(2, java.math.RoundingMode.HALF_UP);
 	}
 }
